fddeb945ea3981f3bd4092debbb4198d5faca7c5
Ashwin Garg <ashwin0173@outlook.com>
2026-02-05 22:38:58 -0500

    raw test

--- test_dummy.py
+++ test_dummy.py
@@ -1,3 +1,14 @@
+import pandas as pd
+import unittest
 
-def test_function():
-    assert 2 * 4 == 8
+class FlightTests(unittest.TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        cls.df = pd.read_csv('output.csv')
+
+    def test_rows(self):
+        self.assertGreaterEqual(len(self.df), 2)
+
+    def test_cols(self):
+        self.assertGreaterEqual(len(self.df.columns), 2)
